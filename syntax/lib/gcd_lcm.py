6f98ee8dc5e1f2c0194622fb4ed9c5139d040714
Jealyang <jaelyangchoi@gmail.com>
2021-02-20 18:41:37 +0900

    문법 활용 예 수정

--- syntax/lib/gcd_lcm.py
+++ syntax/lib/gcd_lcm.py
@@ -1,10 +1,19 @@
 import math
 
+# 최대 공약수(GCD)
+math.gcd(21, 14)  # 7
+
+
+# 유클리드 호제법으로 직접 구하기
+def uclid(a, b):
+    if a % b == 0:
+        return b
+    else:
+        return uclid(b, a % b)
 
 # 최소 공배수(LCM)를 구하는 함수
 def lcm(a, b):
     return a * b // math.gcd(a, b)
 
 
-print(math.gcd(21, 14))  # 7
-print(lcm(21, 14))  # 42
+lcm(21, 14)  # 42
--- syntax/lib/standard.py
+++ syntax/lib/standard.py
@@ -1,5 +1,6 @@
 # 자주 사용되는 내장 함수 : import 필요 x
 result = sum([1, 2, 3, 4, 5])  # 15
+# 아래처럼은 못 씀 sum() takes at most 2 arguments (5 given)
 min_result = min(7, 3, 5, 2)  # 2
 max_result = max(7, 3, 5, 2)  # 7
 
@@ -13,5 +14,3 @@ reverse_result = sorted([9, 1, 8, 5, 4], reverse=True)  # [9, 8, 5, 4, 1]
 array = [('최재량', 29), ('최재림', 26), ('최장환', 56)]
 result = sorted(array, key=lambda t: t[1], reverse=True)  # 튜플의 1번 원소를 기준으로 제시
 print(result)  # [('최장환', 56), ('최재량', 29), ('최재림', 26)]
-
-
