663a421280d75394e5fea01c4d7b3463e428fb3d
Jealyang <jaelyangchoi@gmail.com>
2021-03-03 21:02:34 +0900

    로봇 청소기

--- simulation/BJ14503.py
+++ simulation/BJ14503.py
@@ -4,16 +4,13 @@ back = ((1, 0), (0, -1), (-1, 0), (0, 1))
 n, m = map(int, input().split())
 r, c, d = map(int, input().split())
 a = [list(map(int, input().split())) for _ in range(n)]
-cnt = spin = 0
-stop = False
 while True:
     # 현 위치 청소
     if a[r][c] == 0:
-        cnt += 1
         a[r][c] = 2
     # 왼쪽부터 탐색 진행
     for k in range(4):
-        # 왼쪽 회전
+        # 왼쪽 회전 후 전진
         nr, nc = r + steps[d][0], c + steps[d][1]
         d = d - 1 if d != 0 else 3
         if not (0 <= nr < n and 0 <= nc < m):
@@ -30,5 +27,9 @@ while True:
             r, c = nr, nc
         else:
             break
-
+cnt = 0
+for i in range(n):
+    for j in range(m):
+        if a[i][j] == 2:
+            cnt += 1
 print(cnt)
