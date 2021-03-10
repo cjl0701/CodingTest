27b23e9faee55fe97aa997776576ef5b97ea3357
Jealyang <jaelyangchoi@gmail.com>
2021-03-10 16:46:16 +0900

    정렬 문제에 역시 힙 많이 쓰이는 구나

---
+++ sort/_BJ1715.py
@@ -0,0 +1,14 @@
+# 카드 정렬하기
+import heapq
+
+n = int(input())
+h = []
+for _ in range(n):
+    heapq.heappush(h, int(input()))
+
+total = 0
+while len(h) > 1:
+    s = heapq.heappop(h) + heapq.heappop(h)
+    total += s
+    heapq.heappush(h, s)
+print(total)
