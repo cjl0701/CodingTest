4859f6708866a5b1bf90ab691fb8119a61a4c3b7
Jealyang <jaelyangchoi@gmail.com>
2021-03-14 18:17:17 +0900

    실수 가능성 줄이기 배열들이 아니라 문자열로 표현하는 스킬!

---
+++ brute_force/BJ3019.py
@@ -0,0 +1,47 @@
+# 테트리스
+# 구현하게 쉽게 기능을 분할해라
+# 높이 차가 맞아야 한다.
+
+def calc(i, s):
+    if i + len(s) > c:
+        return 0
+    base = a[i] - (ord(s[0]) - ord('0'))
+    for j in range(len(s)):
+        if base != a[i + j] - (ord(s[j]) - ord('0')):
+            return 0
+    return 1
+
+
+c, p = map(int, input().split())
+a = list(map(int, input().split()))
+ans = 0
+# 모든 자리에 놓아본다.
+for i in range(c):
+    # case별 문자열로 표현하는 스킬
+    if p == 1:
+        ans += calc(i, "0") + calc(i, "0000")  # 바닥만 문자열로 간단하게!
+    elif p == 2:
+        ans += calc(i, "00")
+    elif p == 3:
+        ans += calc(i, "001") + calc(i, "10")
+    elif p == 4:
+        ans += calc(i, "100") + calc(i, "01")
+    elif p == 5:
+        ans += calc(i, "000") + calc(i, "01") + calc(i, "101") + calc(i, "10")
+    elif p == 6:
+        ans += calc(i, "000") + calc(i, "00") + calc(i, "011") + calc(i, "20")
+    elif p == 7:
+        ans += calc(i, "000") + calc(i, "00") + calc(i, "110") + calc(i, "02")
+print(ans)
+""" 이건 실수할 가능성이 높다.
+tet = [
+    [],
+    [(1, 1, 1, 1)],  # 예외처리: l 인경우는 무조건 성공
+    [(1, 1)],
+    [(1, 1, 2), (2, 1)],
+    [(2, 1, 1), (1, 2)],
+    [(1, 1, 1), (1, 2), (2, 1, 2), (2, 1)],
+    [(1, 1, 1), (1, 1), (1, 2, 2), (3, 1)],
+    [(1, 1, 1), (1, 3), (2, 2, 1), (1, 1)]
+]
+"""
