c5b6e4382b4d4033a84af128212dbb9f1ccc5f12
Jealyang <jaelyangchoi@gmail.com>
2021-03-19 23:00:13 +0900

    누적합 => 메모. 중복 계산을 피한다

---
+++ 기타/구간 합 구하기 4(누적합).py
@@ -0,0 +1,13 @@
+# BJ 11659 : 수 n개가 주어졌을 때 i번째~j번째 수까지 합
+""" 누적합 => 메모. 중복 계산을 피한다. """
+# S[i] = A[1]+A[2]+...+A[i] / S[i] = S[i-1]+A[i]
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+a = [0] + list(map(int, sys.stdin.readline().split()))
+s = [0] * (n + 1)
+for i in range(1, n + 1):
+    s[i] = s[i - 1] + a[i]
+for _ in range(m):
+    i, j = map(int, sys.stdin.readline().split())
+    sys.stdout.write(str(s[j] - s[i - 1]) + "\n")
