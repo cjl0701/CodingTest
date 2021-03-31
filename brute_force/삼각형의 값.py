7d274726885f759f3e35a3d65e72c8791e375fd6
Jealyang <jaelyangchoi@gmail.com>
2021-03-31 13:25:04 +0900

    case 분류하되 비슷한 연산이므로 한 함수에서 처리(분기)

--- brute_force/삼각형의 값.py
+++ brute_force/삼각형의 값.py
@@ -1,62 +1,38 @@
-# BJ 4902 https://www.acmicpc.net/problem/4902
-""" 치명적인 실수 : 문제 설명, 값의 범위 잘 읽자..
-1. n=3까지만 고려해서 문제를 잘 못 이해..
-2. 어디서 틀렸는지 모르겠다 => 알고보니 초기값이 0이 아니라 -10000이어야.. 리턴값도..
-3. 전체 갯수 못 구하겠다.. 너무 수학이야 => 갯수 못구했으면 중복 피해야지
-"""
-""" skill
-1. 쫄지말고 case 분류해서 하나씩 작은 문제를 해결하면 풀린다.
-2. 중복을 연산을 피하기 위한 누적합 => s[0] 비워두기
-3. 비슷한 연산인데 조건에 따라 다른 부분이 있으면 한 함수에서 if문 쓰면 된다..
-"""
-import sys
+def calc(row, left, right, s):
+    # 범위 연산은 공통이므로
+    if not (1 <= row <= h) or left < 1 or right >= 2 * row:
+        return
+    s += sum[row][right] - sum[row][left - 1]
+    global ans
+    if ans < s:
+        ans = s
+    if left % 2 == 1:  # 홀수
+        calc(row + 1, left, right + 2, s)
+    else:  # 짝수
+        calc(row - 1, left - 2, right, s)
 
 
-def down(row, left, right, upper):
-    if row >= n or right >= len(a[row]):
-        return 0  # 최하값이어야 한다..
-    cur = upper + s[row][right]
-    if left - 1 >= 0:
-        cur -= s[row][left - 1]
-    return max(cur, down(row + 1, left, right + 2, cur))
-
-
-def up(row, left, right, lower):
-    if row < 0 or left < 0 or right >= len(a[row]):
-        return 0
-    cur = lower + s[row][right] - s[row][left - 1]
-    return max(cur, up(row - 1, left - 2, right, cur))
-
-
-t = 1
+tc = 0
 while True:
-    temp = list(map(int, sys.stdin.readline().split()))
-    n = temp[0]
-    if n == 0:
+    tc += 1
+    inputs = list(map(int, input().split()))
+    h = inputs[0]
+    if h == 0:
         break
-    # 배열로 표현
-    a = [[] for _ in range(n)]
-    idx, length = 1, 1
-    for i in range(n):
-        a[i] = temp[idx:idx + length]
-        idx += length
-        length += 2
+    # 리스트, 누적합
+    a = [[]]  # 빈 리스트 a[0] 뒤에 이어 붙인다
+    sum = [[]]
+    k = 1
+    for i in range(1, h + 1):
+        a.append([0] * (2 * i)) # 그때그때 초기화
+        sum.append([0] * (2 * i))  # 누적합 계산하기 편하도록 0을 비운다
+        for j in range(1, 2 * i):
+            a[i][j] = inputs[k]
+            k += 1
+            sum[i][j] = sum[i][j - 1] + a[i][j]
 
-    ans = 0
-    # 누적합 구해두기
-    s = [[0] * (2 * n - 1) for _ in range(n)]
-    for i in range(len(a)):
-        for j in range(len(a[i])):
-            if j - 1 >= 0:
-                s[i][j] = s[i][j - 1]
-            s[i][j] += a[i][j]
-
-    # 각 시작점에 대하여, case 별로 구하기
-    for i in range(n):
-        for j in range(len(a[i])):
-            if j % 2 == 0:
-                ans = max(ans, down(i, j, j, 0))
-            else:
-                ans = max(ans, up(i, j, j, 0))
-    print(str(t) + ". " + str(ans))
-    t += 1
+    ans = -1000
+    for i in range(1, h + 1):
+        for j in range(1, 2 * i):
+            calc(i, j, j, 0)
+    print(str(tc) + ". " + str(ans))
