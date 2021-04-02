ed2328d17581b91c5abf416b764071446df5c5fd
Jealyang <jaelyangchoi@gmail.com>
2021-04-02 22:54:34 +0900

    heapq 사용법

--- heap/더 맵게.py
+++ heap/더 맵게.py
@@ -1,1 +1,21 @@
 # 프로그래머스
+import heapq
+
+
+def solution(scoville, K):
+    cnt = 0
+    heapq.heapify(scoville)
+    while scoville:
+        scv = heapq.heappop(scoville)
+        if scv >= K:
+            return cnt
+        if scoville:
+            scv += heapq.heappop(scoville) * 2
+            cnt += 1
+            heapq.heappush(scoville, scv)
+    return -1
+
+
+scoville = [1, 2, 3, 9, 10, 12]
+K = 7
+print(solution(scoville, K))
