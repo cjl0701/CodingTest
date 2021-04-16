7d0a0c72c62c4d48ff591e94b3c65ffb369ae6c8
Jealyang <jaelyangchoi@gmail.com>
2021-04-16 13:13:41 +0900

    최소부터 선택 -> 정렬 사이클이 발생하면 최소 x -> 사이클 검사 by 유니온파인드

--- tree/_MST.py
+++ tree/_MST.py
@@ -1,1 +1,38 @@
 # 최소 스패닝 트리 https://www.acmicpc.net/problem/1197
+# kruskal
+
+
+# 대표 찾아 반환
+def find_parent(parent, x):
+    if x != parent[x]:  # 대표는 자신 = 부모
+        parent[x] = find_parent(parent, parent[x])  # 거슬러 올라가 찾음, 경로압축
+    return parent[x]  # 대표를 가리킴
+
+
+# 대표 번호가 작은 쪽 밑으로 들어감
+def union_parent(parent, v1, v2):
+    p1, p2 = parent[v1], parent[v2]
+    if p1 < p2:
+        parent[p2] = p1  # parent[p2]==p2 -> p1
+    else:
+        parent[p1] = p2
+
+
+v, e = map(int, input().split())
+edges = []
+for _ in range(e):
+    v1, v2, cost = map(int, input().split())
+    edges.append((cost, v1, v2))
+
+# 사이클을 생성하지 않는 최소 간선 v-1개 선택하면 됨 (사이클 형성하면 최소 비용 아님)
+edges.sort()
+result = 0
+parent = [i for i in range(v + 1)]
+
+for cost, v1, v2 in edges:
+    # 서로소 집합이면(소속이 다르면), 사이클 형성 x
+    if find_parent(parent, v1) != find_parent(parent, v2):
+        result += cost
+        union_parent(parent, v1, v2)
+
+print(result)
