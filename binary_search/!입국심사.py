e06c29449e6b6797914e8caba82da3c7e681a1b9
Jealyang <jaelyangchoi@gmail.com>
2021-05-07 23:00:36 +0900

    n이 너무 크면 파라메트릭 서치를 고려해보자

--- binary_search/!입국심사.py
+++ binary_search/!입국심사.py
@@ -1,9 +1,11 @@
 # https://programmers.co.kr/learn/courses/30/lessons/43238
-# 파라메트릭 서치: 최적해를 찾아가는 문제
-# 1. 답: 최적 시간
-# 2. 답의 범위를 줄여간다.
-# 3. 그 시간에 n명 처리가 가능한가?
-
+# n이 너무 크다. 완탐 x.. 너무 크다.. 이진 탐색? 파라메트릭 서치!
+"""
+파라메트릭 서치: 최적해를 찾아가는 문제
+1. 답: 최적 시간
+2. 답의 범위를 줄여간다.
+3. 그 시간에 n명 처리가 가능한가?
+"""
 def possible(n, times, time_limit):
     cnt = 0
     for take_time in times:
@@ -12,13 +14,13 @@ def possible(n, times, time_limit):
 
 
 def solution(n, times):
-    start, end = 0, max(times) * n
+    start, end = 1, max(times) * n
     optimal = 0
 
     while start <= end:
         mid = (start + end) // 2
-        if possible(n, times, mid):
-            optimal = mid # 점점 최적해로 다가간다.
+        if possible(n, times, mid):  # 성공하면 시간을 줄이는 방향으로 간다
+            optimal = mid  # 점점 최적해로 다가간다.
             end = mid - 1  # 지금보다 더 줄여본다.
         else:
             start = mid + 1  # 지금보다 늘려야 한다.
