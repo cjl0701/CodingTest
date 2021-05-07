3e4f771bf80afbd805ebfbb7a530bb3c1fcaca14
Jealyang <jaelyangchoi@gmail.com>
2021-05-07 18:10:15 +0900

    약속: 반환 값이 양수면 앞놈을 뒤로 보낸다

--- sort/가장 큰 수.py
+++ sort/가장 큰 수.py
@@ -1,7 +1,7 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42746?language=python3#
 from functools import cmp_to_key
 
-""" 약속
+""" 약속 : 반환 값이 양수면 앞놈을 뒤로 보낸다 
 * 정렬했을 때 a가 b보다 더 뒤에 있어야 한다면 0보다 큰 수를 반환해야 한다
 * 정렬했을 때 a가 b보다 더 앞에 있어야 한다면 0보다 작은 수를 반환해야 한다
 * 정렬했을 때 a와 b가 동등하다면 0을 반환해야 한다.
@@ -9,16 +9,13 @@ from functools import cmp_to_key
 
 
 def compare(x, y):
-    print(x, y)
-    print(int(y + x), int(x + y))
     return int(y + x) - int(x + y)
 
 
 def solution(numbers):
     numbers = list(map(str, numbers))
     numbers.sort(key=cmp_to_key(compare))
-    print(numbers)
-    return ''.join(numbers)
+    return str(int(''.join(numbers)))
 
 
 print(solution([0, 0, 100]))
