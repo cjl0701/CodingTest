b47daf901327a9dbef9ed5027b2a1a836e600293
Jealyang <jaelyangchoi@gmail.com>
2021-05-13 18:00:59 +0900

    DP 문제는 점화식 정의 방법이 가장 중요하다 1. 최종 답을 구하기 쉬운 점화식 2. 이전 단계의 답으로 이뤄지는 점화식

--- dynamic_programming/가장 큰 정사각형.py
+++ dynamic_programming/가장 큰 정사각형.py
@@ -1,16 +1,25 @@
-# D를 어떻게 정의하느냐가 정말 중요하다
+# https://www.acmicpc.net/problem/1915
+""" DP 문제는 점화식 정의 방법이 가장 중요하다
+1. 최종 답을 구하기 쉬운 점화식
+2. 이전 단계의 답으로 이뤄지는 점화식
+"""
 import sys
 
 n, m = map(int, input().split())
-a = ['0' * (m + 1)] + ['0' + sys.stdin.readline().rstrip() for _ in range(n)]
-# 가로, 세로, 대각선 방향 모두 1로 채워져야 한다.
-# 끝 점에서 (가로, 세로, 대각선) 중 최소 길이를 구하자.
-d = [[0] * (m + 1) for _ in range(n + 1)]
+n, m = n + 1, m + 1
+arr = ['0' * m]
+for _ in range(n - 1):
+    arr.append('0' + sys.stdin.readline().rstrip())
+
+# d[i][j]: i,j를 끝으로, 가능한 정사각형의 길이
+d = [[0] * m for _ in range(n)]
 ans = 0
-for i in range(1, n + 1):
-    for j in range(1, m + 1):
-        if a[i][j] == '1':
-            d[i][j] = min(d[i - 1][j], d[i - 1][j - 1], d[i][j - 1]) + 1
+for i in range(1, n):
+    for j in range(1, m):
+        # 끝 점에서 (가로, 세로, 대각선) 중 최소 길이를 구하자.
+        if arr[i][j] == '1':
+            d[i][j] = min(d[i - 1][j - 1], d[i][j - 1], d[i - 1][j]) + 1
         if ans < d[i][j]:
             ans = d[i][j]
+
 print(ans ** 2)
