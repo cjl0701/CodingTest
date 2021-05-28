c250ae4af72ecd34d4648283e6abd031883563b2
Jealyang <jaelyangchoi@gmail.com>
2021-05-28 22:37:30 +0900

    docs: 주석을 더 구체적으로 서술

--- binary_search/!입국심사.py
+++ binary_search/!입국심사.py
@@ -6,6 +6,8 @@
 2. 답의 범위를 줄여간다.
 3. 그 시간에 n명 처리가 가능한가?
 """
+
+
 def possible(n, times, time_limit):
     cnt = 0
     for take_time in times:
@@ -13,6 +15,7 @@ def possible(n, times, time_limit):
     return cnt >= n
 
 
+# 가능하다면 줄이고, 너무 많이 줄였으면 그것보다는 늘리고!
 def solution(n, times):
     start, end = 1, max(times) * n
     optimal = 0
@@ -21,8 +24,8 @@ def solution(n, times):
         mid = (start + end) // 2
         if possible(n, times, mid):  # 성공하면 시간을 줄이는 방향으로 간다
             optimal = mid  # 점점 최적해로 다가간다.
-            end = mid - 1  # 지금보다 더 줄여본다.
+            end = mid - 1  # 최소 값을 찾는 것이 목적이므로, 값의 범위를 줄여간다.
         else:
-            start = mid + 1  # 지금보다 늘려야 한다.
+            start = mid + 1  # 지금보다는 늘려야만 한다.
 
     return optimal
