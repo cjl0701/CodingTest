e7b459908f5acb60676d8b594770130c21134e65
Jealyang <jaelyangchoi@gmail.com>
2021-06-15 18:07:53 +0900

    rechallenge: 틀렸던 문제 다시 풀기

--- brute_force/리모컨.py
+++ brute_force/리모컨.py
@@ -1,49 +1,27 @@
-# BJ 1107
-# 배울 점 0: 재귀 설계부터 -> 애매하면 반복문
-# 배울 점 1: 화끈하게 최대 범위 다 돌려버리면 쉽다.
-# 배울 점 2: 예외 처리 일반화하기
-# 고칠 점: 하나의 예외를 위해 예외 판단을 매번 하는걸 두려워 말자. 그걸 피하려다 틀린다.
-""" 틀린 이유: 반례가 너무 많다
-    => 새로운 로직(성급한 일반화인 경우) or 예외 처리
-    *예외 처리 실패 이유
-    1. 예외 처리가 복잡해서 방황
-    2. 예외 처리 일반화 못함.. 로직에 녹여야 한다. """
+# 빠른 원 큐 풀이법: 극단적 예시를 먼저 들어놓고 구현해야 한다. 소잃고 외양간 고치기 x
 
-# 재귀 설계
-# 탈출 조건: x자리 ->x+1자리
-# 성공: x자리 -> (x-1,x,x+1) 자리
-# 연산: 고장나지 않았으면 누름
-# 반환: x
-# 예외 : 000=0..자릿수만 차지..
-# 재귀가 애매하다 -> 반복문
-# 어디까지 반복? 1,000,000-100
-
-n = int(input())
-m = int(input())
-broken = [False] * 10  # 0~9번
-if m != 0:
-    for x in map(int, input().split()):
-        broken[x] = True
-# +,-만 눌러 이동할 경우
-ans = abs(n - 100)
-
-
-# 채널로 이동 -> +,-만 누르기
-def possible(ch):
-    if ch == 0:
-        return 0 if broken[0] else 1
+def is_possible(channel, broken):
+    if channel == 0:
+        return -1 if broken[channel] else 1
     press = 0
-    while ch > 0:
-        if broken[ch % 10]:
-            return 0
-        ch //= 10
+    while channel > 0:
         press += 1
+        if broken[channel % 10]:
+            return -1
+        channel //= 10
     return press
 
 
-for ch in range(1000000 - 100):
-    press = possible(ch)
-    if press > 0:
-        ans = min(ans, abs(n - ch) + press)
-
+goal = int(input())
+broken = [False] * 10
+if input() != '0':  # 0일 경우 다음 읽을 것이 없다 -> EOF Exception
+    for x in map(int, input().split()):
+        broken[x] = True
+# +,- 누르는 경우
+ans = abs(goal - 100)
+# 번호 누르는 경우
+for channel in range(max(goal * 2 - 100, 100)):
+    press = is_possible(channel, broken)
+    if press != -1:
+        ans = min(ans, press + abs(goal - channel))
 print(ans)
