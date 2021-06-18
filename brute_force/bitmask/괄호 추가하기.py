69580e3a8bd516f4cf5ecdd14fc6ae880b7afbac
Jealyang <jaelyangchoi@gmail.com>
2021-06-18 15:41:50 +0900

    refactor: 인덱스 사용이 덜 복잡하게 resolve

--- brute_force/bitmask/괄호 추가하기.py
+++ brute_force/bitmask/괄호 추가하기.py
@@ -1,79 +1,49 @@
 """
-괄호를 적절히 추가해 최댓값 만들기
-괄호 안에는 연산자가 하나만.
-중첩 괄호 불가.
+[틀린 이유]
+엣지케이스 -> input 값이 아니라 result가 엣지일 수 있음.(초기값, 없는 값에 주의)
+복잡할수록 예외 걸릴 확률이 높다. 특히 인덱스 복잡.. => 쉽게 계산하도록 꾀를 부리자!
+[접근법]
+괄호 => 먼저 계산한다.
+먼저 계산할 것들을 정하고(비트마스크) 먼저 계산한다.
+순차적으로 계산한다.
+꾀: 계산상 편의를 위해 '먼저 계산한 결과', '+', '0'으로 치환한다.
 """
 
-"""틀린 이유 
-엣지케이스 -> input 값이 아니라 result가 엣지일 수 있음.(초기값, 없는 값에 주의)
-복잡할수록 예외 걸릴 확률이 높다. 특히 인덱스 복잡..
-"""
-from collections import deque
+
+def operate(operand1, operand2, operator):
+    if operator == '*':
+        return operand1 * operand2
+    elif operator == '+':
+        return operand1 + operand2
+    else:
+        return operand1 - operand2
+
 
 n = int(input())
-m = 0
-operator = list()
-operand = list()
-for op in input():
-    if op.isdigit():
-        operand.append(int(op))
-    else:
-        operator.append(op)
-        m += 1
-ans = operand[0] if n == 1 else None
-
-
-def valid(bitmask, length, order):
-    before = False
-    for i in range(length):
-        if (1 << i & bitmask) > 0:
-            if before:
-                return False
-            else:
-                before = True
-                order.add(i)
-        else:
-            before = False
-    return True
-
-
-def operate(operator, operand, prio):
-    operator_q = deque()
-    operand_q = deque()
-    i = 0
-    while i < len(operator):
-        if i in prio:
-            if operator[i] == '+':
-                operand_q.append(operand[i] + operand[i + 1])
-            elif operator[i] == '-':
-                operand_q.append(operand[i] - operand[i + 1])
-            elif operator[i] == '*':
-                operand_q.append(operand[i] * operand[i + 1])
-            if i + 1 < len(operator):
-                operator_q.append(operator[i + 1])
-            i += 2
-        else:
-            operand_q.append(operand[i])
-            operator_q.append(operator[i])
-            i += 1
-    if i == len(operator):
-        operand_q.append(operand[i])
-    ret = operand_q.popleft()
-    while operand_q:
-        op = operator_q.popleft()
-        if op == '+':
-            ret += operand_q.popleft()
-        elif op == '-':
-            ret -= operand_q.popleft()
-        elif op == '*':
-            ret *= operand_q.popleft()
-    return ret
-
-
-for bm in range(1 << m):
-    order = set()
-    if valid(bm, m, order):
-        result = operate(operator, operand, order)
+m = (n - 1) // 2
+expression = list(input())
+for i in range(0, n, 2):
+    expression[i] = int(expression[i])
+ans = None
+for bitmask in range(1 << m):
+    # 먼저 계산할 것(괄호)를 정한다
+    valid = True
+    for i in range(m - 1):
+        if bitmask & (1 << i) > 0 and bitmask & (1 << (i + 1)) > 0:
+            valid = False
+    # 먼저 계산한다
+    if valid:
+        ex = expression[:]
+        for k in range(m):
+            if bitmask & (1 << k) > 0:
+                i = 2 * k + 1
+                ex[i - 1] = operate(ex[i - 1], ex[i + 1], ex[i])
+                ex[i] = '+'
+                ex[i + 1] = 0
+        # 최종 계산
+        result = ex[0]
+        for i in range(1, n, 2):
+            result = operate(result, ex[i + 1], ex[i])
         if ans is None or ans < result:
             ans = result
 print(ans)
