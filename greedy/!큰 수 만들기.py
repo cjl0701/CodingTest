8d81035f04a05fcfc8ccdafe8627800648bc73cc
Jealyang <jaelyangchoi@gmail.com>
2021-06-19 12:53:20 +0900

    rechallenge: 틀렸던 문제 다시 풀기

--- greedy/!큰 수 만들기.py
+++ greedy/!큰 수 만들기.py
@@ -3,15 +3,16 @@
 1. 시간 초과가 나면 시간 복잡도를 줄이려고 해라. O(N)으로 될까?
 2. 그리디라고 스택, 힙 등 자료구조 안 쓰는건 아니다. 시간 복잡도를 줄이기 위해 필요.
 """
+"""앞에 작은 놈이 없어야"""
+
+
 # 정당성: 앞 자리의 수가 큰 수가 큰 수이다.
 # 앞에 큰 수를 유지하며 k개를 빼야한다.
 def solution(number, k):
-    stack=[number[0]]
-    for num in number[1:]:
-        # 앞에서부터 큰 수만 담아야 한다. 작은 수 k개를 뺀다.
-        while k>0 and stack and stack[-1]<num:
+    stack = list()
+    for new in number:
+        while stack and k > 0 and stack[-1] < new:
             stack.pop()
-            k-=1
-        stack.append(num)
-
-    return ''.join(stack[:len(stack)-k])
+            k -= 1
+        stack.append(new)
+    return ''.join(stack[:len(stack) - k])
